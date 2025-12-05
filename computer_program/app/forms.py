18274823d372580b8b1874bd5584bccb4d682470
Culver22 <o.culver1@newcastle.ac.uk>
2025-12-05 12:47:47 +0000

    added a method: repeated_sequence, which checks if a password has 3 repeated characters in a row

--- computer_program/app/forms.py
+++ computer_program/app/forms.py
@@ -4,4 +4,22 @@ from wtforms import StringField, PasswordField, SubmitField, TextAreaField
 from wtforms.validators import (DataRequired, Length, EqualTo, ValidationError, Regexp)
 from flask import current_app
 
-Password_Blacklist = {"Password123$", "Qwerty123!", "Adminadmin1@", "weLcome123!"}
+password_blacklist = {"Password123$", "Qwerty123!", "Adminadmin1@", "weLcome123!"}
+
+# regex follows normal email format: 'oliverculver234@gmail.com'
+email_regex = r"^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$"
+
+def repeated_sequence(password, repeat_len = 3):
+    # loop through each character in a password
+    for i in range(len(password) - (repeat_len - 1)):
+        repeated = True
+        # loop through the next character and compare to i
+        for j in range(1, repeat_len):
+            if password[i] != password[i + j]:
+                repeated = False
+                break
+
+        if repeated:
+            return True
+
+    return False
