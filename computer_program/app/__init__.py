a3cd7df3015b02f302c26774e3e0cd3dc1e4d464
Culver22 <o.culver1@newcastle.ac.uk>
2025-12-07 16:42:24 +0000

    Added error handling in creat_app.

--- computer_program/app/__init__.py
+++ computer_program/app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from config import Config
 
@@ -13,21 +13,34 @@ def create_app():
     from .routes import main
     app.register_blueprint(main)
 
+    @app.errorhandler(400)
+    def bad_request(error):
+        return render_template('400.html'), 400
+
+    @app.errorhandler(403)
+    def forbidden(error):
+        # user does not have permission to access content
+        return render_template('403.html'), 403
+
+    @app.errorhandler(404)
+    def page_not_found(error):
+        # page doesn't exist
+        return render_template('404.html'), 404
+
+    @app.errorhandler(500)
+    def internal_server_error(error):
+        # error within the server, don't show debug info
+        return render_template('500.html'), 500
+
     with app.app_context():
-        from .models import User
         db.drop_all()
         db.create_all()
 
-        users = [
-            {"username": "user1@email.com", "password": "Userpass!23", "role": "user", "bio": "I'm a basic user"},
-            {"username": "mod1@email.com", "password": "Modpass!23", "role": "moderator", "bio": "I'm a moderator"},
-            {"username": "admin1@email.com", "password": "Adminpass!23", "role": "admin", "bio": "I'm an administrator"}
-        ]
-
-        for user in users:
-            user = User(username=user["username"], password=user["password"], role=user["role"], bio=user["bio"])
-            db.session.add(user)
-            db.session.commit()
+        from .models import User
+        db.session.add(User("admin@example.com", "Admin123!", "admin", "Admin user."))
+        db.session.add(User("moderator@example.com", "Mod123!!!", "moderator", "Moderator user."))
+        db.session.add(User("user@example.com", "User123!!!", "user", "Normal user."))
+        db.session.commit()
 
     return app
 
