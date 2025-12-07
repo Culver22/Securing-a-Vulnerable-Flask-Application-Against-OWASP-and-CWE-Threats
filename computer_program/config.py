093e6c9a4292c28a9d75f25bc259f46e4dbfa513
Culver22 <o.culver1@newcastle.ac.uk>
2025-12-07 13:01:05 +0000

    updated config file, so that the Fernet Key and Password Pepper can be utilised in models. Currently, the Fernet is hardcoded (to allow the app to run) in production it will be set via environment variables

--- computer_program/config.py
+++ computer_program/config.py
@@ -3,3 +3,12 @@ class Config:
     SECRET_KEY = 'supersecretkey'
     SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    PASSWORD_PEPPER = "dev-pepper-change-me"
+    # hard coded Fernet key, to allow the app to run
+    # for actual implementation must be set via environment variables
+    FERNET_KEY = "dev-fernet-key-change-me"
+
+    SESSION_COOKIE_HTTPONLY = True
+    SESSION_COOKIE_SAMESITE = "Lax"
+    SESSION_COOKIE_SECURE = False  # set true when using HTTPS
