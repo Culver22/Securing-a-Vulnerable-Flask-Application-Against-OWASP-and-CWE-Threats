7b1451a95d30be60fa43440330535240f1f64cd0
Culver22 <o.culver1@newcastle.ac.uk>
2025-12-08 12:59:35 +0000

    Updated Config, to have both a debug mode with hard coded keys and then the production mode with loaded environment variables

--- computer_program/config.py
+++ computer_program/config.py
@@ -1,14 +1,44 @@
+from datetime import timedelta
+import os
+
 class Config:
-    DEBUG = True
-    SECRET_KEY = 'supersecretkey'
+
+    # SQLite db
     SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    PASSWORD_PEPPER = "dev-pepper-change-me"
-    # hard coded Fernet key, to allow the app to run
-    # for actual implementation must be set via environment variables
-    FERNET_KEY = "dev-fernet-key-change-me"
-
+    # Session security flags
     SESSION_COOKIE_HTTPONLY = True
     SESSION_COOKIE_SAMESITE = "Lax"
-    SESSION_COOKIE_SECURE = False  # set true when using HTTPS
+
+    # expire session after 30 minutes
+    PERMANENT_SESSION_LIFETIME = timedelta(minutes=30)
+
+class DevConfig(Config):
+    DEBUG = True
+
+    # assign session cookies only hard-coded for coursework
+    SECRET_KEY = 'dev-secret-key-change-me'
+
+    # used for hashing passwords not stored in the db
+    PASSWORD_PEPPER = "dev-pepper-change-me"
+
+    # hard coded to ensure the app runs
+    FERNET_KEY = "dev-fernet-key-change-me"
+
+    SESSION_COOKIE_SECURE = False
+
+class ProdConfig(Config):
+    DEBUG = False
+    # allow cookies over HTTPS
+    SESSION_COOKIE_SECURE = True
+
+    # load secrets from environment
+    SECRET_KEY = os.environ.get("SECRET_KEY", "")
+    PASSWORD_PEPPER = os.environ.get("PASSWORD_PEPPER", "")
+    FERNET_KEY = os.environ.get("FERNET_KEY", "")
+
+    # Fail if any key cannot be loaded
+    if not SECRET_KEY or not PASSWORD_PEPPER or not FERNET_KEY:
+        raise RuntimeError("Production secrets must be set in environment variables!")
+
