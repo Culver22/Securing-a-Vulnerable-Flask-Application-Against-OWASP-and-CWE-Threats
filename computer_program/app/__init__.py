a4659b5aa7dd65db15556bb27313ab210b71e365
Culver22 <o.culver1@newcastle.ac.uk>
2025-12-08 13:10:22 +0000

    Set the app to be created and run in DevConfig (Development mode

--- computer_program/app/__init__.py
+++ computer_program/app/__init__.py
@@ -1,12 +1,12 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
-from config import Config
+from config import DevConfig
 
 db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_object(Config)
+    app.config.from_object(DevConfig)
 
     db.init_app(app)
 
